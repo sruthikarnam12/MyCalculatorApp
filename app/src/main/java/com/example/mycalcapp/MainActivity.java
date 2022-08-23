f59ba106920119a231d0e11362fa2ab22b25188f
sruthikarnam12 <90780347+sruthikarnam12@users.noreply.github.com>
2022-08-23 11:41:04 -0500

    worked on code for buttons but still need to finish that up

--- app/src/main/java/com/example/mycalcapp/MainActivity.java
+++ app/src/main/java/com/example/mycalcapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mycalcapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void findAns(View view){
+
     }
 
 }
