515e81a55757fb59e46903de251fab56e0db2bea
sruthikarnam12 <90780347+sruthikarnam12@users.noreply.github.com>
2022-08-24 10:51:43 -0500

    the code for the buttons functionality is done

--- app/src/main/java/com/example/mycalcapp/MainActivity.java
+++ app/src/main/java/com/example/mycalcapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,8 +17,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void findAns(View view){
+    // this adds together the 2 numbers that the user inputs in the calculator app,
+    // so this gives you the sum of the two numbers
+    public void findSum(View view){
+        TextInputEditText firstNum = findViewById(R.id.firstNum);
+        TextInputEditText secondNum = findViewById(R.id.secondNum);
+        TextView sumOf = findViewById(R.id.result);
 
+        int num1 = Integer.parseInt(firstNum.getText().toString());
+        int num2 = Integer.parseInt(secondNum.getText().toString());
+        int sum = num1 + num2;
+
+        sumOf.setText("" + sum);
+    }
+
+    // this subtracts the 2 numbers that the user inputs in the calculator app.
+    // so it will give you the difference of the two
+    public void findDifference(View view){
+        TextInputEditText firstNum = findViewById(R.id.firstNum);
+        TextInputEditText secondNum = findViewById(R.id.secondNum);
+        TextView sumOf = findViewById(R.id.result);
+
+        int num1 = Integer.parseInt(firstNum.getText().toString());
+        int num2 = Integer.parseInt(secondNum.getText().toString());
+        int sum = num1 - num2;
+
+        sumOf.setText("" + sum);
+    }
+
+
+    // this will multiply the two numbers the user inputs together,
+    // giving you the product of the two
+    public void findProduct(View view){
+        TextInputEditText firstNum = findViewById(R.id.firstNum);
+        TextInputEditText secondNum = findViewById(R.id.secondNum);
+        TextView sumOf = findViewById(R.id.result);
+
+        int num1 = Integer.parseInt(firstNum.getText().toString());
+        int num2 = Integer.parseInt(secondNum.getText().toString());
+        int sum = num1 * num2;
+
+        sumOf.setText("" + sum);
+    }
+
+    // this will divide the two numbers the user inputs together,
+    // giving you the quotient of the two
+    public void findQuotient(View view){
+        TextInputEditText firstNum = findViewById(R.id.firstNum);
+        TextInputEditText secondNum = findViewById(R.id.secondNum);
+        TextView sumOf = findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(firstNum.getText().toString());
+        double num2 = Integer.parseInt(secondNum.getText().toString());
+        double sum = num1 / num2;
+
+        sumOf.setText("" + sum);
     }
 
 }
